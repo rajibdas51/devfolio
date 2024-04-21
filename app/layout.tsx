d48caa4714f3aee0c3484592faf8ebe8cfd4bc38
Rajib Chandra Das <prorajib6@gmail.com>
2024-04-21 18:12:23 +0600

    add Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         <div className='bg-[#ffddda] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#c8c4e9] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
       </body>
     </html>
