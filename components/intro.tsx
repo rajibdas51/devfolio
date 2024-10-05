710a4ba30c7f089ed82d0d02b6aeb878087e7f2c
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-05 19:31:09 +0600

    add about and section-heading component

--- components/intro.tsx
+++ components/intro.tsx
@@ -28,7 +28,7 @@ export default function Intro() {
         </motion.div>
       </div>
 
-      <motion.p
+      <motion.h1
         className='mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-4xl '
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -38,7 +38,7 @@ export default function Intro() {
         <span className='font-bold'>1.5 years</span> of experience. I enjoy
         building <span className='italic'>interactive web apps</span>. My focus
         is <span className='underline'>React (Next.js) and express</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className='flex flex-col sm:flex-row items-center justify-center gap-2'
