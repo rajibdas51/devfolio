afa36795253602b289a18821de6366458298763c
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-05 20:25:59 +0600

    add Projects component

--- components/about.tsx
+++ components/about.tsx
@@ -15,9 +15,9 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className='mb-3'>
-        After graduating with a degree in{' '}
-        <span className='font-medium'>Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{' '}
+        Hello, I'm Rajib and I recently graduated with a degree in{' '}
+        <span className='font-medium'>Computer Science & Engineering.</span> I
+        have a deep passion for coding and problem-solving, with a focus on{' '}
         <span className='font-medium'>full-stack web development</span>.{' '}
         <span className='italic'>My favorite part of programming</span> is the
         problem-solving aspect. I <span className='underline'>love</span> the
@@ -26,19 +26,19 @@ export default function About() {
         <span className='font-medium'>
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{' '}
+        . I am also familiar with TypeScript,sql and Prisma. I am always looking
+        to learn new technologies. I am currently looking for a{' '}
         <span className='font-medium'>full-time position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className='italic'>When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{' '}
-        <span className='font-medium'>learning new things</span>. I am currently
-        learning about{' '}
-        <span className='font-medium'>history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className='italic'>When I'm not coding</span>, I find joy in
+        expressing my creativity through painting, and I love spending my free
+        time watching movies and{' '}
+        <span className='font-medium'>learning new things</span>. I'm excited
+        about the endless possibilities in the dynamic field of web development
+        and am eager to contribute my skills to building extraordinary projects.{' '}
       </p>
     </motion.section>
   );
