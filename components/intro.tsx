223ba9e0ef979823abc7fe835dd810f646814e6c
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-07 22:00:40 +0600

    refactor intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,11 +41,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I'm Rajib.</span> I'm a{' '}
-        <span className='font-bold'>full-stack developer</span> with{' '}
-        <span className='font-bold'>1.5 years</span> of experience. I enjoy
-        building <span className='italic'>interactive web apps</span>. My focus
-        is <span className='underline'>React (Next.js) and express</span>.
+        <span className='font-bold'>Hello, I'm Rajib,</span> a passionate{' '}
+        <span className='font-bold'>full-stack developer.</span> . I enjoy
+        solving problems and building{' '}
+        <span className='italic'>interactive web apps</span>. My focus is{' '}
+        <span className='underline'>React (Next.js) and express</span>.
       </motion.h1>
 
       <motion.div
