7e6ef1922fffa6fb0437bef87cde26a923ff1812
Rajib <prorajib6@gmail.com>
2025-06-26 18:03:52 +0600

    fix buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -74,21 +74,24 @@ export default function Intro() {
           Download CV{' '}
           <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
-        <a
-          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://www.linkedin.com/in/rajib-chandra-das/'
-          target='_blank'
-        >
-          <BsLinkedin />
-        </a>
 
-        <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://github.com/rajibdas51/'
-          target='_blank'
-        >
-          <FaGithubSquare />
-        </a>
+        <div className='flex flex-row  gap-3'>
+          <a
+            className='bg-white p-5 text-xl text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+            href='https://www.linkedin.com/in/rajib-chandra-das/'
+            target='_blank'
+          >
+            <BsLinkedin />
+          </a>
+
+          <a
+            className='bg-white text-xl p-5 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+            href='https://github.com/rajibdas51/'
+            target='_blank'
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
       </motion.div>
     </section>
   );
